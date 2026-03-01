4039f49f67b2228f5cebe09d9404707edac13bae
hello1231223134123-rgb <tanhoantan7659@gmail.com>
2026-03-01 15:55:10 +0700

    b12_13

---
+++ b12_13/prac_13.py
@@ -0,0 +1,17 @@
+def tingluong(giolam, luonggio):
+    return giolam * luonggio
+def inhthongin(ten, giolam, luongg, tongluong):
+    print("Ten:", ten)
+    print("gio lam:",giolam)
+    print("luong:",luongg)
+    print("ton luong:",tongluong)
+tongluong = tingluong(12, 150)
+inhthongin("Nguyen Vam A",12,150,tongluong)
+
+
+
+
+
+
+
+
